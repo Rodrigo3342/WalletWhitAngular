bcd2c855731066110fb2ef232cb8a44689c295a7
Rodrigo <gabriel.valda33@gmail.com>
2019-03-06 13:13:05 -0400

    navegacion

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RegistroComponent } from './registro/registro.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { EstadisticasComponent } from './estadisticas/estadisticas.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // { path: '', component: AppComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'registro', component: RegistroComponent },
+  { path: 'inicio', component: InicioComponent },
+  { path: 'estadistica', component: EstadisticasComponent }
+  // { path: '**', component: AppComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { EstadisticasComponent } from './estadisticas/estadisticas.component';
+import { RegistroComponent } from './registro/registro.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    InicioComponent,
+    EstadisticasComponent,
+    RegistroComponent
   ],
   imports: [
     BrowserModule,
